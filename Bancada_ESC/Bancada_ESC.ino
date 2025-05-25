0cadb5b4fbeeffe070234ad156e5cbee5991d407
Euler Torres <eulertorres@outlook.com>
2025-05-25 08:56:32 -0300

    Não lembro kkkkkkk

--- Bancada_ESC/Bancada_ESC.ino
+++ Bancada_ESC/Bancada_ESC.ino
@@ -9,16 +9,16 @@ byte data, trash;
 void setup() {
   Serial.begin(9600);              //Inicia comunicação serial Bluetooth
     DDRD |= B10000000;             // Porta 7 output
-    DDRB |= B00101111;             // Porta 8, 9, 10, 11, e 13 output
+    //DDRB |= B00101111;             // Porta 8, 9, 10, 11, e 13 output
 
   Serial.println("Envie: 'd' para aumentar 10% | 'a' para diminuir 10% | '0' para parar o motor");
 
   while(Serial.available())data = Serial.read(); //Limpa o buffer serial
   data = 0;                                      //Zera os dados
 
-    PCICR |= (1 << PCIE2);         // Registeador PCIE0  em alto para habilitar scaneamento da interrupção através do PCMSK0
-    PCMSK2 |= (1 << PCINT18);      // Bit 18 do Registrador PCINT0 em alto (entrada digital 2)  para causar uma interrupção em qualquer mudança.
-    PCMSK2 |= (1 << PCINT19);      // Bit 19 do Registrador PCINT0 em alto (entrada digital 3)  para causar uma interrupção em qualquer mudança.
+    //PCICR |= (1 << PCIE2);         // Registeador PCIE0  em alto para habilitar scaneamento da interrupção através do PCMSK0
+    //PCMSK2 |= (1 << PCINT18);      // Bit 18 do Registrador PCINT0 em alto (entrada digital 2)  para causar uma interrupção em qualquer mudança.
+    //PCMSK2 |= (1 << PCINT19);      // Bit 19 do Registrador PCINT0 em alto (entrada digital 3)  para causar uma interrupção em qualquer mudança.
 }
 
 void loop() {
@@ -41,58 +41,18 @@ void loop() {
     }
   }
 
-  if(micros() - loop_timer > 4050)PORTB |= B00100000;   // Se o loop for maior que 4050 us, ligamos  LED de alerta
-  while(micros() - loop_timer < 4000);                  // Garante que teremos 250hz de atualização
+  while(micros() - loop_timer < 20000);                  // Garante que teremos 250hz de atualização
   loop_timer = micros();                                // Zera o contador do loop
   PORTD |= B10000000;                                   // Pulso alto pro ESC
   timer_channel = esc + loop_timer;                     // Define o tempo de alto
 
   while(PORTD >= 64){                                   // Enquanto tiver em alto
-  esc_loop_timer = micros();                            //Lê o tempo atual
-  if(timer_channel <= esc_loop_timer)PORTD &= B01111111;// Quando o atual for maior que estabelecido, coloca em baixo
-  }
-
-  switch (esc) {		//  Lock up table para vizualização com as LEDs
-    case 1000:
-      PORTB = B00000000;
-      break;
-    case 1050:
-      PORTB = B00000001;
-      break;
-    case 1100:
-      PORTB = B00000010;
-      break;
-    case 1150:
-      PORTB = B00000011;
-      break;
-    case 1200:
-      PORTB = B00000100;
-      break;
-    case 1250:
-      PORTB = B00000101;
-      break;
-    case 1300:
-      PORTB = B00000110;
-      break;
-    case 1350:
-      PORTB = B00000111;
-      break;
-    case 1400:
-      PORTB = B00001000;
-      break;
-    case 1450:
-      PORTB = B00001001;
-      break;
-    case 1500:
-      PORTB = B00001010;
-      break;
-    default:
-      PORTB |= B00000000;
-      break;
+	esc_loop_timer = micros();                            //Lê o tempo atual
+	if(timer_channel <= esc_loop_timer)PORTD &= B01111111;// Quando o atual for maior que estabelecido, coloca em baixo
   }
 }
 
-ISR(PCINT2_vect){
+/* ISR(PCINT2_vect){
   if(PIND & B00000100){
     if(esc<1500 & (millis()>button_timer)){
       esc += 50;
@@ -108,3 +68,4 @@ ISR(PCINT2_vect){
     }
   }
 }
+ */
